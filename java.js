ee0af525567add3fa1d14e498002004392fbf720
Davi Arthur <daviarthursistro08@gmail.com>
2023-02-23 18:55:19 -0300

    exercicio-3

---
+++ java.js
@@ -0,0 +1,10 @@
+
+
+function calcular() {
+    let num1 = document.getElementById("num1").value
+    let num2 = document.getElementById("num2").value   
+
+    let resultado = num1 * 100 / num2
+
+    alert(resultado +"%")
+}   
